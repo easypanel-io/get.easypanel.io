dde1569bac3975727c53e02736ab9424a842d396
Andrei Canta <deiucanta@gmail.com>
2022-06-14 16:07:10 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 curl -sSL https://get.docker.com | sh
 
-docker run --rm -it \
+docker run --rm -i \
   -v /etc/easypanel:/etc/easypanel \
   -v /var/run/docker.sock:/var/run/docker.sock:ro \
   easypanel/easypanel setup
