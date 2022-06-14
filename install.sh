7edfdacb1834308f080374ccd3dcdd0ab4718911
Andrei Canta <deiucanta@gmail.com>
2022-06-14 15:16:32 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+curl -sSL https://get.docker.com | sh
+
+docker run --rm -it \
+  -v /etc/easypanel:/etc/easypanel \
+  -v /var/run/docker.sock:/var/run/docker.sock:ro \
+  easypanel/easypanel setup
