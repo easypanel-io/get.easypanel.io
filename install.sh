f567d56a9a60e58d43d7aca080ff528c0729a79c
Andrei Canta <deiucanta@gmail.com>
2026-01-02 18:33:55 +0200

    Refactor Docker installation in install.sh
    Updated Docker installation process to use APT repository and GPG key.

--- install.sh
+++ install.sh
@@ -38,7 +38,26 @@ command_exists() {
 if command_exists docker; then
   echo "Docker already installed"
 else
-  curl -sSL https://get.docker.com | sh
+  # curl -sSL https://get.docker.com | sh
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 fi
 
 docker swarm leave --force 1> /dev/null 2> /dev/null || true
